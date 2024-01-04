f2201cd61be9dbc1410dd621b3234c83c086dbcc
Anandavel1984 <155619391+Anandavel1984@users.noreply.github.com>
2024-01-04 23:38:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-echo "anand";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ValueSERP API</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="assets/css/contact.css">
+<link rel="stylesheet" href="assets/css/demo-responsive.css">
+</head>
+<body>
+
+<div class="ega-contact-layout1">
+<div class="ega-contact-form1-wrapper d-flex justify-content-center align-items-center flex-column position-relative">
+<a href="#" class="contact-logo">
+<img src="assets/images/form-shapes/logo-black.png" alt>
+</a>
+<form action="api.php" method="post" class="contact-form1">
+<h5 class="contact-form-title">ValueSERP API</h5>
+<div class="ege-input-group">
+<label for="user-name">Search Query</label>
+<input type="text" id="sq1" name="sq1" required>
+<p>Search for multiple data separated by comma(,)</p>
+</div>
+<div class="ege-submit-input">
+<input type="submit" value="Search">
+</div>
+</form>
+</div>
+</div>
+
+</body>
+</html>

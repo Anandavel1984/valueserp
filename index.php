b57d13eb75c105bd8947c4a7950c841879f81d85
Anandavel1984 <155619391+Anandavel1984@users.noreply.github.com>
2024-01-06 11:10:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ValueSERP API</title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/css/main.css">
-<link rel="stylesheet" href="assets/css/contact.css">
-<link rel="stylesheet" href="assets/css/demo-responsive.css">
+<link rel="stylesheet" href="bootstrap.min.css">
+<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="contact.css">
+<link rel="stylesheet" href="demo-responsive.css">
 </head>
 <body>
 
 <div class="ega-contact-layout1">
 <div class="ega-contact-form1-wrapper d-flex justify-content-center align-items-center flex-column position-relative">
 <a href="#" class="contact-logo">
-<img src="assets/images/form-shapes/logo-black.png" alt>
+<img src="logo-black.png" alt>
 </a>
 <form action="api.php" method="post" class="contact-form1">
 <h5 class="contact-form-title">ValueSERP API</h5>
